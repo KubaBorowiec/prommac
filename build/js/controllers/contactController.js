712fe59753d63d4137975d42cba4d4ab68eb286b
Kuba <jakub.borow@gmail.com>
2017-05-18 00:25:17 +0200

    contact form validater

--- build/js/controllers/contactController.js
+++ build/js/controllers/contactController.js
@@ -1,19 +1,21 @@
 app.controller("contactController",["$scope",function($scope){
- $scope.pageClass = 'contact';
- $scope.required = true;
- $scope.check = function(type) {
-   var input = $('input.'+type);
-   var value = $('input.'+type).val();
-   if( value == null || value.length == 0){
-   	  input.removeClass('green');
-   	  input.addClass('red');
-   	  input.next('.message').text("Uzupełnij pole");
-   }
-   else{
-   	 input.removeClass('red');
-   	 input.addClass('green');
-   	 input.next('.message').text("");
-   }
- };
+	$scope.pageClass = 'contact';
+	$scope.required = true;
+	$scope.check = function(required,type) {
+	   var input = $('input.'+type);
+	   var value = $('input.'+type).val();
+	    if(required == 1){
+			    if( value == null || value.length == 0){
+			   	  	input.removeClass('valid');
+			   	  	input.addClass('notvalid');
+			   	  	input.next('.message').text("Uzupełnij pole");
+			    }
+			    else{
+			   	 	input.removeClass('notvalid');
+			   	 	input.addClass('valid');
+			   	 	input.next('.message').text("");
+			    }
+	    };
+	}
 
 }]);

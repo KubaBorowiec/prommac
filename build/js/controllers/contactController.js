b92bc1e21de47025b53247432268251d796e2568
Kuba <jakub.borow@gmail.com>
2017-05-17 13:13:07 +0200

    follow changed in contact form

--- build/js/controllers/contactController.js
+++ build/js/controllers/contactController.js
@@ -1,4 +1,19 @@
 app.controller("contactController",["$scope",function($scope){
- $scope.pageClass = 'contact'
+ $scope.pageClass = 'contact';
+ $scope.required = true;
+ $scope.check = function(type) {
+   var input = $('input.'+type);
+   var value = $('input.'+type).val();
+   if( value == null || value.length == 0){
+   	  input.removeClass('green');
+   	  input.addClass('red');
+   	  input.next('.message').text("Uzupełnij pole");
+   }
+   else{
+   	 input.removeClass('red');
+   	 input.addClass('green');
+   	 input.next('.message').text("");
+   }
+ };
 
 }]);
